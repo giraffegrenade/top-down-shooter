7e3a36779fb27f9b6b095db6bde72758fc3e8790
as14g18 <as14g18@soton.ac.uk>
2019-01-24 20:23:51 +0000

    Enemy now follows player

--- TopDownShooter/scenes/Naga.gd
+++ TopDownShooter/scenes/Naga.gd
@@ -3,6 +3,6 @@ extends KinematicBody2D
 const SPEED = 100.0    
 
 func _physics_process(delta):
-    var direction = ($Player.position - position).normalized()
-    var motion = direction * SPEED * delta
-    position += motion
+    var direction = (get_parent().get_node("Player").position - position).normalized()
+    var motion = direction * SPEED
+    move_and_slide(motion)
